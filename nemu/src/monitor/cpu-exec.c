20ee1fb2477fe4f5ba0c597a0653437c86d8af4d
tracer-ics2017 <tracer@njuics.org>
2021-04-11 17:05:01 +0800

    >  compile 161920202 sunzhe Linux bogon 4.19.0-14-686-pae #1 SMP Debian 4.19.171-2 (2021-01-30) i686 GNU/Linux  17:05:01 up 24 min,  1 user,  load average: 0.00, 0.00, 0.00 888347062d2dc7fffa8066d8df7ed70a9db8837

--- nemu/src/monitor/cpu-exec.c
+++ nemu/src/monitor/cpu-exec.c
@@ -1,6 +1,6 @@
 #include "nemu.h"
 #include "monitor/monitor.h"
-
+#include "monitor/watchpoint.h"
 /* The assembly code of instructions executed is only output to the screen
  * when the number of instructions executed is less than this value.
  * This is useful when you use the `si' command.
@@ -9,7 +9,8 @@
 #define MAX_INSTR_TO_PRINT 10
 
 int nemu_state = NEMU_STOP;
-
+WP *scan_watchpoint();
+uint32_t expr(char *args, bool success);
 void exec_wrapper(bool);
 
 /* Simulate how the CPU works. */
@@ -29,7 +30,12 @@ void cpu_exec(uint64_t n) {
 
 #ifdef DEBUG
     /* TODO: check watchpoints here. */
-
+{
+	WP *p = scan_watchpoint();
+	if(p != NULL) {
+		nemu_state = NEMU_STOP;
+	}
+}
 #endif
 
 #ifdef HAS_IOE
