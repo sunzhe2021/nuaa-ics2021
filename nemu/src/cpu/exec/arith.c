afc5806944da3a317eea603927ae7639607fbde6
tracer-ics2017 <tracer@njuics.org>
2021-05-19 21:19:27 +0800

    >  compile 161920202 sunzhe Linux bogon 4.19.0-14-686-pae #1 SMP Debian 4.19.171-2 (2021-01-30) i686 GNU/Linux  21:19:27 up  1:41,  2 users,  load average: 0.00, 0.00, 0.00 3b7d5fbb98e4d33b3390fff6d7dc3d3147b7ca7

--- nemu/src/cpu/exec/arith.c
+++ nemu/src/cpu/exec/arith.c
@@ -29,9 +29,16 @@ make_EHelper(sub) {
 }
 
 make_EHelper(cmp) {
-  TODO();
+  rtl_sext(&t1, &id_dest->val, id_dest->width);
+  rtl_sext(&t2, &id_src->val, id_src->width);
+  rtl_sub(&t0, &t1, &t2);
+  t3 = (t0 > t1);
+  rtl_set_CF(&t3);
+  t3 = ((((int32_t)(t1) < 0) == (((int32_t)(t2) >> 31) == 0 ))&&(((int32_t)(t0) < 0) != ((int32_t)(t1) < 0 )));
+  rtl_set_OF(&t3);
+  rtl_update_ZFSF(&t0, 4);
 
-  print_asm_template2(cmp);
+  //print_asm_template2(cmp);
 }
 
 make_EHelper(inc) {
