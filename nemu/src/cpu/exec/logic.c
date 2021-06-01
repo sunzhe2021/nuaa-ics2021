39feaef08819f783368f5b0cd20e1b0e4ffbf658
tracer-ics2017 <tracer@njuics.org>
2021-06-01 12:31:30 +0800

    >  compile 161920202 sunzhe Linux bogon 4.19.0-14-686-pae #1 SMP Debian 4.19.171-2 (2021-01-30) i686 GNU/Linux  12:31:30 up  1:57,  2 users,  load average: 0.08, 0.02, 0.00 8ee4d877d72175feb60f1b96b31c9b399facb85e

--- nemu/src/cpu/exec/logic.c
+++ nemu/src/cpu/exec/logic.c
@@ -83,7 +83,21 @@ make_EHelper(not) {
 }
 
 make_EHelper(rol) {
-  TODO();
-  
+  rtl_shl(&t0, &id_dest->val, &id_src->val);
+  if(decoding.is_operand_size_16) {
+	t3 = 0;
+	rtl_addi(&t1, &t3, 16);
+	rtl_sub(&t1, &t1, &id_src->val);
+	rtl_shr(&t2, &id_dest->val, &t1);
+  }
+  else {
+	t3 = 0;
+	rtl_addi(&t1, &t3, 32);
+	rtl_sub(&t1, &t1, &id_src->val);
+	rtl_shr(&t2, &id_dest->val, &t1);
+  }
+  rtl_or(&t0, &t0, &t2);
+  operand_write(id_dest, &t0);
+  rtl_update_ZFSF(&t0, id_dest->width);
   print_asm_template2(rol);
 }
