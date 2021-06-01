7a98fc03248c81930af1439cd1ad6e7948c177dd
tracer-ics2017 <tracer@njuics.org>
2021-06-02 00:39:37 +0800

    >  compile 161920202 sunzhe Linux bogon 4.19.0-14-686-pae #1 SMP Debian 4.19.171-2 (2021-01-30) i686 GNU/Linux  00:39:37 up 11 min,  2 users,  load average: 0.00, 0.02, 0.02 66087498bce100a54dcc30c9304bebfb6ade7d1e

--- nemu/src/memory/memory.c
+++ nemu/src/memory/memory.c
@@ -1,5 +1,6 @@
 #include "nemu.h"
-
+#include "device/mmio.h"
+#include "memory/mmu.h"
 #define PMEM_SIZE (128 * 1024 * 1024)
 
 #define pmem_rw(addr, type) *(type *)({\
@@ -12,11 +13,23 @@ uint8_t pmem[PMEM_SIZE];
 /* Memory accessing interfaces */
 
 uint32_t paddr_read(paddr_t addr, int len) {
-  return pmem_rw(addr, uint32_t) & (~0u >> ((4 - len) << 3));
+  int mmio_n;
+  if((mmio_n = is_mmio(addr)) != -1) {
+	return mmio_read(addr, len, mmio_n);
+  }
+  else {
+	return pmem_rw(addr, uint32_t) & (~0u >> ((4 - len) << 3));
+  }
 }
 
 void paddr_write(paddr_t addr, int len, uint32_t data) {
-  memcpy(guest_to_host(addr), &data, len);
+  int mmio_n;
+  if((mmio_n = is_mmio(addr)) != -1) {
+	mmio_write(addr, len, data, mmio_n);
+  }
+  else {
+	memcpy(guest_to_host(addr), &data, len);
+  }
 }
 
 uint32_t vaddr_read(vaddr_t addr, int len) {
