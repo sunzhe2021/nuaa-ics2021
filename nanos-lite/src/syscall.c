c1c495dc1a81e4f2e40f0f81795e0d4d38b38485
tracer-ics2017 <tracer@njuics.org>
2021-06-08 22:42:46 +0800

    >  run 161920202 sunzhe Linux bogon 4.19.0-14-686-pae #1 SMP Debian 4.19.171-2 (2021-01-30) i686 GNU/Linux  22:42:46 up  3:32,  2 users,  load average: 0.10, 0.03, 0.01 2e3de799596efd1f4e4730cb98990496458bd8

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -36,9 +36,21 @@ static inline uintptr_t sys_brk(uintptr_t new_brk) {
 _RegSet* do_syscall(_RegSet *r) {
   uintptr_t a[4];
   a[0] = SYSCALL_ARG1(r);
+  a[1] = SYSCALL_ARG2(r);
+  a[2] = SYSCALL_ARG3(r);
+  a[3] = SYSCALL_ARG4(r);
 
   switch (a[0]) {
-    default: panic("Unhandled syscall ID = %d", a[0]);
+	case SYS_none: SYSCALL_ARG1(r)= 1;break;	 
+	case SYS_exit:_halt(SYSCALL_ARG2(r));break;
+	case SYS_write : SYSCALL_ARG1(r) = sys_write(a[1],a[2],a[3]);break;
+	case SYS_brk: SYSCALL_ARG1(r) = mm_brk(a[1]);break;
+ 	case SYS_open: SYSCALL_ARG1(r) = sys_open(a[1],a[2],a[3]);break;
+	case SYS_read: SYSCALL_ARG1(r) = sys_read(a[1],a[2],a[3]);break;
+	case SYS_close:SYSCALL_ARG1(r) = sys_close(a[1]);break;
+	case SYS_lseek:  SYSCALL_ARG1(r) = sys_lseek(a[1], a[2], a[3]); break;
+	default: panic("Unhandled syscall ID = %d", a[0]);
+	
   }
 
   return NULL;
