afd95f6e74f672fc234325eee51afcf47d6c36c6
tracer-ics2017 <tracer@njuics.org>
2021-06-08 21:30:03 +0800

    >  run 161920202 sunzhe Linux bogon 4.19.0-14-686-pae #1 SMP Debian 4.19.171-2 (2021-01-30) i686 GNU/Linux  21:30:03 up  2:19,  2 users,  load average: 0.00, 0.00, 0.00 e60e2e7f75919bbd27f03140534f7f27d781fb32

--- nanos-lite/src/main.c
+++ nanos-lite/src/main.c
@@ -30,7 +30,7 @@ int main() {
 
   init_fs();
 
-  uint32_t entry = loader(NULL, "intrid.S");
+  uint32_t entry = loader(NULL, "build");
   ((void (*)(void))entry)();
 
   panic("Should not reach here");
