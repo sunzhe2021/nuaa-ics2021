8b21ec050dc71d066da4eb729fddedab36310239
tracer-ics2017 <tracer@njuics.org>
2021-06-13 12:38:50 +0800

    >  run 161920202 sunzhe Linux bogon 4.19.0-14-686-pae #1 SMP Debian 4.19.171-2 (2021-01-30) i686 GNU/Linux  12:38:50 up 16 min,  2 users,  load average: 0.09, 0.04, 0.02 7c4145d52c10fe9c24b1e0b8b1deb97f3c2e0983

--- nanos-lite/src/loader.c
+++ nanos-lite/src/loader.c
@@ -5,9 +5,9 @@
 extern void ramdisk_read(void *buf, off_t offset, size_t len);
 extern size_t get_ramdisk_size();
 uintptr_t loader(_Protect *as, const char *filename) {
-  ramdisk_read(DEFAULT_ENTRY, 0, get_ramdisk_size());
-  /*
-int fd = fs_open(filename, 0, 0);
+  //ramdisk_read(DEFAULT_ENTRY, 0, get_ramdisk_size());
+  
+  int fd = fs_open(filename, 0, 0);
   size_t nbyte = fs_filesz(fd);
   Log("loaded:[%d]%s size:%d", fd, filename, nbyte);
   void *end = DEFAULT_ENTRY + nbyte;
@@ -19,7 +19,7 @@ int fd = fs_open(filename, 0, 0);
 	fs_read(fd, pa, (end-va)<PGSIZE ? (end - va) : PGSIZE);
 	Log("Map va to pa: 0x%08x to 0x%08x", va, pa);
   }
-*/
+
   //fs_read(fd, DEFAULT_ENTRY, lens);
   //fs_close(fd);
   return (uintptr_t)DEFAULT_ENTRY;
