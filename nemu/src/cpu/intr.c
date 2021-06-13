f683ec40e0d8e12122d657d2e8c227d27711dee2
tracer-ics2017 <tracer@njuics.org>
2021-06-13 22:32:23 +0800

    >  compile 161920202 sunzhe Linux bogon 4.19.0-14-686-pae #1 SMP Debian 4.19.171-2 (2021-01-30) i686 GNU/Linux  22:32:23 up 10:10,  2 users,  load average: 0.00, 0.00, 0.00 b30fd069b020a57fc9fe64a998672025356514a

--- nemu/src/cpu/intr.c
+++ nemu/src/cpu/intr.c
@@ -6,16 +6,18 @@ void raise_intr(uint8_t NO, vaddr_t ret_addr) {
    * That is, use ``NO'' to index the IDT.
    */
   rtl_push(&cpu.eflags.val);
-  cpu.eflags.IF = 0;
   rtl_push(&cpu.cs);
   rtl_push(&ret_addr);
-  t0 = cpu.idtr.base;
-  t1 = t0 + NO * 8;
-  t0 = vaddr_read(t1, 2);
-  t2 = vaddr_read(t1 + 4, 4);
-  t1 = t0 + (t2 & 0xffff0000);
-  decoding.is_jmp = 1;
-  decoding.jmp_eip = t1;
+  uint32_t low, high;
+  if(NO <= cpu.idtr.limit ) {
+	low = vaddr_read(cpu.idtr.base + NO * 8, 4 ) & 0xffff;
+	high = vaddr_read(cpu.idtr.base + NO * 8 + 4, 4 ) & 0xffff0000;
+	decoding.is_jmp = 1;
+	decoding.jmp_eip = low + high;
+  }
+  else {
+	assert(0);
+  }
 }
 
 void dev_raise_intr() {
