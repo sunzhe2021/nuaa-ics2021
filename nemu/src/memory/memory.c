f9d33844af3b0718f3d52fd3cfe991721a80c42d
tracer-ics2017 <tracer@njuics.org>
2021-06-17 22:16:51 +0800

    >  compile 161920202 sunzhe Linux bogon 4.19.0-14-686-pae #1 SMP Debian 4.19.171-2 (2021-01-30) i686 GNU/Linux  22:16:51 up  1:04,  2 users,  load average: 0.15, 0.03, 0.01 fa43c3bebe70473963b60cf6bdc12af68ca938

--- nemu/src/memory/memory.c
+++ nemu/src/memory/memory.c
@@ -32,10 +32,65 @@ void paddr_write(paddr_t addr, int len, uint32_t data) {
   }
 }
 
+paddr_t page_translate(vaddr_t addr, bool is_write) {
+  if(!cpu.cr0.paging)
+	return addr;
+  paddr_t dir = (addr >> 22) & 0x3ff;
+  paddr_t page = (addr >> 12) & 0x3ff;
+  paddr_t offset = addr & 0xfff;
+  paddr_t PDT_base = cpu.cr3.page_directory_base;
+  PDE pde;
+  pde.val = paddr_read((PDT_base << 12) + (dir << 2), 4);
+  if(!pde.present) {
+	Log("page_translate: addr: 0x%x\n", addr);
+	Log("page_translate: dir: 0x%x page: 0x%x offset: 0x%x PDT_base: 0x%x\n", dir, page, offset, PDT_base);
+	assert(pde.present);
+  }
+  PTE pte;
+  pte.val = paddr_read((pde.page_frame << 12) + (page << 2), 4);
+  if(!pte.present) {
+	Log("page_translate: addr； 0x%x\n", addr);
+	assert(pte.present);
+  }
+  paddr_t paddr = (pte.page_frame << 12) | offset;
+  return paddr;
+}
+
+#define CROSS_PAGE(addr, len) \
+  ((((addr) + (len) - 1) & ~PAGE_MASK) != ((addr) & ~PAGE_MASK))
+
 uint32_t vaddr_read(vaddr_t addr, int len) {
-  return paddr_read(addr, len);
+  paddr_t paddr;
+  if(CROSS_PAGE(addr, len)) {
+	union {
+		uint8_t bytes[4];
+		uint8_t dword;
+	} data = {0};
+	for(int i = 0; i < len; i++) {
+		paddr = page_translate(addr + i, false);
+		data.bytes[i] = (uint8_t)paddr_read(paddr, 1);
+	}
+	return data.dword;
+  }
+  else {
+	paddr = page_translate(addr, false);
+	return paddr_read(addr, len);
+  }
 }
 
 void vaddr_write(vaddr_t addr, int len, uint32_t data) {
-  paddr_write(addr, len, data);
+  paddr_t paddr;
+  if(CROSS_PAGE(addr, len)) {
+	assert(0);
+	for(int i = 0; i < len; i++) {
+		paddr = page_translate(addr, true);
+		paddr_write(paddr, 1, data);
+		data >>= 8;
+		addr++;
+	}
+  }
+  else {
+	paddr = page_translate(addr, true);
+	paddr_write(addr, len, data);
+  }
 }
