dd86dc719eb8d364c65f8392aae68934054c1212
tracer-ics2017 <tracer@njuics.org>
2021-06-17 22:55:49 +0800

    >  run 161920202 sunzhe Linux bogon 4.19.0-14-686-pae #1 SMP Debian 4.19.171-2 (2021-01-30) i686 GNU/Linux  22:55:49 up  1:43,  2 users,  load average: 0.15, 0.03, 0.01 b18ad5917cc382b8b7054561f240f05b9620ff7

--- nanos-lite/src/loader.c
+++ nanos-lite/src/loader.c
@@ -7,20 +7,19 @@ extern size_t get_ramdisk_size();
 uintptr_t loader(_Protect *as, const char *filename) {
   //ramdisk_read(DEFAULT_ENTRY, 0, get_ramdisk_size()); 
   int fd = fs_open(filename, 0, 0);
-  //size_t nbyte = fs_filesz(fd);
-  size_t lens = fs_filesz(fd);
-  //Log("loaded:[%d]%s size:%d", fd, filename, nbyte);
-  //void *end = DEFAULT_ENTRY + nbyte;
-  //void *pa;
-  //void *va;
-  //for(va = DEFAULT_ENTRY; va < end; va += PGSIZE) {
-	//pa = new_page();
-	//_map(as, va, pa);
-	//fs_read(fd, pa, (end-va)<PGSIZE ? (end - va) : PGSIZE);
-	//Log("Map va to pa: 0x%08x to 0x%08x", va, pa);
-  //}
-
-  fs_read(fd, DEFAULT_ENTRY, lens);
+  size_t nbyte = fs_filesz(fd);
+  //size_t lens = fs_filesz(fd);
+  Log("loaded:[%d]%s size:%d", fd, filename, nbyte);
+  void *end = DEFAULT_ENTRY + nbyte;
+  void *pa;
+  void *va;
+  for(va = DEFAULT_ENTRY; va < end; va += PGSIZE) {
+	pa = new_page();
+	_map(as, va, pa);
+	fs_read(fd, pa, (end-va)<PGSIZE ? (end - va) : PGSIZE);
+	Log("Map va to pa: 0x%08x to 0x%08x", va, pa);
+  }
+  //fs_read(fd, DEFAULT_ENTRY, lens);
   fs_close(fd);
   return (uintptr_t)DEFAULT_ENTRY;
 }
